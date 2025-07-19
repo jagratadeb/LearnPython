ec402d3e09ce5d6fcf97099b3e1906e27a41c59b
Jagrata Deb <jagratadeb2004@gmail.com>
2025-07-19 15:57:35 +0530

    Fix typos

--- 11 Functions/04keywordArguments.py
+++ 11 Functions/04keywordArguments.py
@@ -2,4 +2,4 @@ def print_details(**kwargs):
     for key, value in kwargs.items():
         print(f"{key} : {value}")
 
-print_details(name="Jagrata Deb", age= "20", country="Indi")
+print_details(name="Jagrata Deb", age= "20", country="India")
