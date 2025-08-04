90f1592f32182b404346f71a5b666d5e4732013c
Jagrata Deb <jagratadeb2004@gmail.com>
2025-08-04 22:00:56 +0530

    Improvements

---
+++ 01 Hello World/02print.py
@@ -0,0 +1,10 @@
+# This is a simple print statement in Python
+# This snippet prints multiple items on the same line
+print("Mango","Orange","Guava","Banana")
+
+# This snippet prints multiple items on the same line with a custom separator
+print("Mango","Orange","Guava","Banana", sep=",")
+
+# This snippet prints multiple items on the same line with a custom end character
+print("Mango","Orange","Guava","Banana",end=", ")
+print("Mango","Orange","Guava","Banana")
