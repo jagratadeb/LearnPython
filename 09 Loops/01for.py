f0b365bcafb322cba9e33ad44d160ba2e00614c0
Jagrata Deb <jagratadeb2004@gmail.com>
2025-08-05 01:44:25 +0530

    update for loop

--- 09 Loops/01for.py
+++ 09 Loops/01for.py
@@ -1,18 +1,32 @@
 # For loop example
 for i in range(5):
     print("for loop iteration:", i)
-
 print("\n")
 
 for i in range(1,11):
     print(f"1 X {i} = {1*i}")
-
 print("\n")
 
 for i in range(1,11,2):
     print(f"{i}")
-
-print('\n')
+print("\n")
 
 for i in range(5,-1,-1):
     print(i)
+print("\n")
+
+for i in "Python":
+    print(i)
+print("\n")
+
+for i in [1, 2, 3, 4, 5]:
+    print(f"Number: {i}")
+print("\n")
+
+for i in (10, 20, 30):
+    print(f"Tuple value: {i}")
+print("\n")
+
+for i in {1, 2, 3}:
+    print(f"Set value: {i}")
+print("\n")
