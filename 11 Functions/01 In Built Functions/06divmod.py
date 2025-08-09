05a0a5a942b9d56eef5963745e6b76eca36f3463
Jagrata Deb <jagratadeb2004@gmail.com>
2025-08-09 22:54:41 +0530

    fix comments

--- 11 Functions/01 In Built Functions/06divmod.py
+++ 11 Functions/01 In Built Functions/06divmod.py
@@ -1,3 +1,3 @@
-print(divmod(10, 3))  # Returns (3, 1) which is (10 // 3, 10 % 3
-print(divmod(20, 6))  # Returns (3, 2) which is (20 // 6, 20 % 6
+print(divmod(10, 3))  # Returns (3, 1) which is (10 // 3, 10 % 3)
+print(divmod(20, 6))  # Returns (3, 2) which is (20 // 6, 20 % 6)
 print(divmod(100, 25))  # Returns (4, 0) which is (100 // 25, 100 % 25)
