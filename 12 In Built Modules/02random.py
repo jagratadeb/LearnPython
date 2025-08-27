85acb7c6497add1a9950badb9e446eb2aef10d64
Jagrata Deb <jagratadeb2004@gmail.com>
2025-08-27 15:57:19 +0530

    Update directory sequencing

--- 12 In Built Modules/02random.py
+++
@@ -1,7 +0,0 @@
-import random
-
-print(random.randint(1, 100)) # Generates a random integer between 1 and 100
-print(random.random()) # Generates a random float between 0.0 and 1.0
-print(random.choice(['apple', 'banana', 'cherry'])) # Randomly selects an element from a list
-print(random.sample(range(1, 101), 5)) # Randomly selects 5 unique elements from a range from 1 to 100
-print(random.uniform(1.0, 10.0)) # Generates a random float between 1.0 and 10.0
