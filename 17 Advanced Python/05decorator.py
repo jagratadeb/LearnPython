a6965a14ba16458864a939c760acb2dc5561b71e
Jagrata Deb <jagratadeb2004@gmail.com>
2025-11-26 13:01:35 +0530

    add code for decorators

---
+++ 17 Advanced Python/05decorator.py
@@ -0,0 +1,15 @@
+# Step 1: Define a decorator
+def my_decorator(func):
+    def wrapper():
+        print("Before the function runs...")
+        func()   # call the original function
+        print("After the function runs...")
+    return wrapper
+
+# Step 2: Use the decorator
+@my_decorator
+def say_hello():
+    print("Hello!")
+
+# Step 3: Call the decorated function
+say_hello()
