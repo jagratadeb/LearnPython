1b734775130e1b24c483cdf847fe8f8a7bdc1a49
Jagrata Deb <jagratadeb2004@gmail.com>
2025-11-29 23:22:19 +0530

    update code

--- 23 Flask/Jinja/jinja.py
+++ 23 Flask/Jinja/jinja.py
@@ -2,7 +2,7 @@
 # Variable Rule
 # Jinja 2 Template Engine
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect
 
 # WSGI Application
 app = Flask(__name__)
@@ -19,12 +19,12 @@ def index():
 def about():
     return render_template("about.html")
 
-@app.route('/submit',methods=['GET','POST'])
-def submit():
-    if request.method == 'POST':
-        name = request.form['name']
-        return f"Hello {name}!"
-    return render_template("form.html")
+# @app.route('/submit',methods=['GET','POST'])
+# def submit():
+#     if request.method == 'POST':
+#         name = request.form['name']
+#         return f"Hello {name}!"
+#     return render_template("form.html")
 
 # Variable Rule
 @app.route('/success/<int:score>')
@@ -53,6 +53,17 @@ def successres(score):
 def successif(score):
     return render_template('result.html',results=score)
 
+
+@app.route('/submit',methods=['POST','GET'])
+def submit():
+    total_score = 0
+    if request.method == "POST":
+        science = float(request.form['science'])
+        maths = float(request.form['maths'])
+        history = float(request.form['history'])
+
+        total_score = (science + maths + history)/3
+    return redirect(url_for('successres', score = total_score))
 
 if __name__ == "__main__":
     app.run(debug=True)
